b43112a5b68f7d91b105352aefe6a81dc618d3c9
ratfactor <devnull@localhost>
2015-01-22 19:31:34 -0700

    Composer package and PSR-4 autoloader setup attempt

---
+++ src/HummingJay.php
@@ -0,0 +1,8 @@
+<?php 
+namespace Ratfactor\HummingJay;
+
+class HummingJay {
+	public function helloworld(){
+		return "Hello, just making sure I have a proper Composer package with PSR-4 autoloading set up correctly.";
+	}
+}
